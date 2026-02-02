d879ba2f91e59fadc8a89a85d417641910e49eec
BsHeR4 <bshermahayni@gmail.com>
2026-02-02 23:09:14 +0300

    feat(Core): add SetLocaleLang middleware for dynamic request localization
    - Implement middleware to detect language preference from `Accept-Language` header.
    - Set application locale dynamically per request to support multi-lingual responses.
    - Default to English (`en`) if no header is provided.
    - Provide a scalable foundation for localized event tags and system messages.

---
+++ app/Http/Middleware/SetLocaleLang.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Middleware to set the application's language based on the request header
+ *
+ * This checks the 'Accept-Language' header from the incoming request
+ * If the header isn't provided, it defaults to English ('en')
+ */
+class SetLocaleLang
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Get the locale from the 'Accept-Language' header, or default to 'en'
+        $locale = $request->header('Accept-Language', 'en');
+
+        // Set the application's locale for the current request
+        app()->setLocale($locale);
+
+        return $next($request);
+    }
+}
